1ad58225a596f2892217acc5e5a4275c21fa1f51
Dinkal12 <ansh24275@gmail.com>
2026-09-08 23:09:45 +0530

    authentication

--- node.js/project2/routes/staticRouter.js
+++ node.js/project2/routes/staticRouter.js
@@ -4,10 +4,21 @@ const URL = require("../models/url");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const allUrls = await URL.find({});
+    // req.user is set by checkAuth middleware (may be undefined if not logged in)
+    if (!req.user) return res.redirect("/login");
+    const allUrls = await URL.find({ createdBy: req.user._id });
     return res.render("home", {
         urls: allUrls,
+        user: req.user,
     });
+});
+
+router.get("/signup", (req, res) => {
+    return res.render("SignUp", { error: req.query.error || null });
+});
+
+router.get("/login", (req, res) => {
+    return res.render("login", { error: req.query.error || null });
 });
 
 module.exports = router;
